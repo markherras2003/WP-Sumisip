2d8d452ed95b89b64f80d01e974974a7b7049607
markherras2003 <larrymark2003@gmail.com>
2020-01-29 20:51:06 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -493,6 +493,24 @@ require get_template_directory() . '/inc/template-tags.php';
     
 */
 
+
+
+function excerpt($limit) {
+	$excerpt = explode(' ', get_the_excerpt(), $limit);
+	if (count($excerpt)>=$limit) {
+	  array_pop($excerpt);
+	  $excerpt = implode(" ",$excerpt).'...';
+	} else {
+	  $excerpt = implode(" ",$excerpt);
+	}	
+	$excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+	return $excerpt;
+  }
+
+
+
+
+
 add_action( 'show_user_profile', 'extra_user_profile_fields' );
 add_action( 'edit_user_profile', 'extra_user_profile_fields' );
 
