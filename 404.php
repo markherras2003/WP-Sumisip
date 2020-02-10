851fb5d555e627964698d73494e89bbd54454728
Al-Rayyan Abdurahman <rayyana00@gmail.com>
2020-02-11 02:42:27 +0800

    404

--- 404.php
+++ 404.php
@@ -1,33 +1,15 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
-
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<div class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</div><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+	<?php get_header(); ?>
+    <section class="page-404">
+        <div class="global-wrapper section-padding">
+            <div class="text-404">
+                <h1>404</h1>
+                <h1 class="display-1 light">Page not found.</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum.</p>
+                <a href="<?= get_home_url(); ?>" class="dark">Go Home</a>
+            </div>
+            <div class="sumisip-brand">
+                <img src="<?= get_template_directory_uri(); ?>/assets/images/svg/branding-symbol.svg">
+            </div>
+        </div>
+    </section>
+    <?php  get_footer(); ?>
