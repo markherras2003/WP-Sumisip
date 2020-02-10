3e8f357d3fcbec3e36cbd75a856680f6e81e4f0c
Al-Rayyan Abdurahman <rayyana00@gmail.com>
2020-02-10 19:25:41 +0800

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,11 +1,15 @@
-<form role="search" method="get" class="search-form search-sidebar" action="<?php echo home_url( '/' ); ?>">
-    <label style="width: 100%">
-        <input type="search" class="search-field search"
-            placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>"
+<section>
+    <form role="search" method="get" class="search-form search-sidebar" action="<?php echo home_url( '/' ); ?>"> 
+        <div class="input-group w-icon icon-right margin-top-2">
+            <div class="icon-wrapper">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path
+                        d="M508.5 481.6l-129-129c-2.3-2.3-5.3-3.5-8.5-3.5h-10.3C395 312 416 262.5 416 208 416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c54.5 0 104-21 141.1-55.2V371c0 3.2 1.3 6.2 3.5 8.5l129 129c4.7 4.7 12.3 4.7 17 0l9.9-9.9c4.7-4.7 4.7-12.3 0-17zM208 384c-97.3 0-176-78.7-176-176S110.7 32 208 32s176 78.7 176 176-78.7 176-176 176z" />
+                </svg>
+            </div>
+            <input id="input-sample-1" type="text" placeholder="Search for stuffs and things..." placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>"
             value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-    </label>
-
-    <button type="submit" class="search-icon-wrapper"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg></button>
-
-</form>
+            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" >
+        </div>
+    </form>
+</section>
