3bf6c8a675ec127565ad71c8168fa41dcffb11ac
Al-Rayyan Abdurahman <rayyana00@gmail.com>
2020-02-10 19:26:29 +0800

    Update post-search.php

--- partials/posts/post-search.php
+++ partials/posts/post-search.php
@@ -2,60 +2,84 @@
     global $post;
     $author_ID = $post->post_author;
 ?>
-    
-    <div class="posts-item">
+<main>
+    <h1 class="search-title margin-bottom-4">Search Results for: <?php the_search_query(); ?></h1>
 
-    <h1 class="search-title">Search Results for: <?php the_search_query(); ?></h1>
-
-    <?php while( have_posts() ) :  ?>
-        <?php the_post(); ?>
+    <?php 
+        $i=0;
+        while( have_posts() ) :  ?>
+        <?php 
+        the_post(); ?>
         <div class="post-item">
             <div class="post-img">
             <?php
                 if( has_post_thumbnail()) {
-                    the_post_thumbnail();
-                } else {
+                    if ($i+1 === 1) {
+
+               
                     ?>
-                    <img src="<?= get_template_directory_uri(); ?>/assets/images/no-available.png">
+
+            <div class="post-details">
+            <div class="post-title">
+                <a href="<?= get_permalink();?>">
+                    <h3 class="post-title"> <?php the_title() ?></h3>
+                </a>
+                <div class="post-etc">
+                    <span><?= strtoupper(get_author_role($author_ID)); ?><?php //get_user_meta($author_ID)['first_name'][0]?></span>
+                    <span><?= get_the_date(); ?></span>
+                    <span><?= get_the_category()[0]->name ?></span>
+                </div>
+            </div>
+                    
+            <?php } ?>
+                    
+                    
+                  <?=  ( ($i+1) === 1 )? the_post_thumbnail('',array('class'=> 'photo-effect')): the_post_thumbnail()?>
+            <?php } else {
+                    ?>
+                    <img src="<?= get_template_directory_uri(); ?>/assets/images/no-available.png" class="photo-effect">
                     <?php
                 }
+                $i++;
             ?>
             </div>
-            <!-- <div class="post-details"> -->
-            <div class="post-title-group">
+
+            <?php if ($i > 1) { ?>
+        <div class="post-details">
+            <div class="post-title">
                 <a href="<?= get_permalink();?>">
                     <h3 class="post-title"><?php the_title() ?></h3>
                 </a>
                 <div class="post-etc">
-                    <span><?= get_user_meta($author_ID)['first_name'][0]?></span>
+                    <span><?= strtoupper(get_author_role($author_ID)); ?><?php //get_user_meta($author_ID)['first_name'][0]?></span>
                     <span><?= get_the_date(); ?></span>
-                    <span><?=  get_the_category()[0]->name ?></span>
+                    <span><?= get_the_category()[0]->name ?></span>
                 </div>
-            </div>
-
-            <div class="post-content-group">
-                <?php the_excerpt(); ?>
-
-                <a href="<?= get_permalink();?>" class="read-more">Read Story</a>
-            </div>
-            
-
-            <!-- </div> -->
         </div>
 
+        <?php } ?>
 
+            <div class="post-content">
+                <?=  the_excerpt(); ?>
+                <a href="<?= get_permalink();?>" class="button outline black">Read Story</a>
+            </div>
+
+        </div>
+
+    </div>
 
     <?php  endwhile; ?>
 
-<div class="pagination">
-    <a href="#" class="previous-pagination">Previous</a>
-    <div class="numeric-pagination">
-        <a href="#">1</a>
-        <a href="#" class="active">2</a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-    </div>
-    <a href="#" class="next-pagination">Next</a>
+    <div class="pagination">
+        <a href="#" class="previous-pagination">Previous</a>
+        <div class="numeric-pagination">
+            <a href="#">1</a>
+            <a href="#" class="active">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <a href="#">5</a>
+        </div>
+        <a href="#" class="next-pagination">Next</a>
 </div>
-</div>
+
+            </main>
