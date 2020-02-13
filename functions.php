2a3b428ae3cd1324264656534e0fd7f0cd486a9b
bolante93 <48409161+bolante93@users.noreply.github.com>
2020-02-13 14:43:07 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -726,7 +726,7 @@ function title_filter( $where, $wp_query ){
 
 add_filter( 'rest_authentication_errors', 'sumisip_api_request_ips_allowed' );
 function sumisip_api_request_ips_allowed( $errors ){
-    $allowed_ips = array( 'localhost', '192.168.0.251' );
+    $allowed_ips = array( 'localhost', '192.168.0.251', '45.13.132.15' );
     $request_server = $_SERVER['REMOTE_ADDR'];
     if( ! in_array( $request_server, $allowed_ips ) )
         return new WP_Error( 'forbidden_access', 'Access denied', array( 'status' => 403 ) );
