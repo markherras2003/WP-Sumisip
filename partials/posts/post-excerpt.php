01fb7504acc3f8a328f96ca9201d93421aae1fd7
markherras2003 <larrymark2003@gmail.com>
2020-02-15 01:57:08 +0800

    Update post-excerpt.php

--- partials/posts/post-excerpt.php
+++ partials/posts/post-excerpt.php
@@ -17,7 +17,7 @@
         'post_type' => 'post',
         'posts_per_page' => 5,
         'paged' =>$paged,
-        'showposts'=>0,
+        'showposts'=>100,
          
     );
     $post_excerpt = new \WP_Query($arg);
