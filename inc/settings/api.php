eccb3d86d793babd37a75795f5a7e2d7743cfabe
bolante93 <48409161+bolante93@users.noreply.github.com>
2020-02-17 17:20:11 +0800

    Update api.php

--- inc/settings/api.php
+++ inc/settings/api.php
@@ -31,14 +31,14 @@ function slug_get_feed_featured_image_post_meta_cb( $object, $field_name, $reque
  * @param $errors WP_Error
  * @return WP_Error
  */
-function sumisip_api_request_ips_allowed( $errors ){
-    $allowed_ips = array( 'localhost', '192.168.0.251', '45.13.132.15' );
-    $request_server = $_SERVER['REMOTE_ADDR'];
-    if( ! in_array( $request_server, $allowed_ips ) )
-        return new WP_Error( 403, 'Not Allowed to access gateway. Contact Dev-Ops for more info.' );
-    return $errors;
-}
-add_filter( 'rest_authentication_errors', 'sumisip_api_request_ips_allowed' );
+//function sumisip_api_request_ips_allowed( $errors ){
+//    $allowed_ips = array( 'localhost', '192.168.0.251', '45.13.132.15' );
+//    $request_server = $_SERVER['REMOTE_ADDR'];
+//    if( ! in_array( $request_server, $allowed_ips ) )
+//        return new WP_Error( 403, 'Not Allowed to access gateway. Contact Dev-Ops for more info.' );
+//    return $errors;
+//}
+//add_filter( 'rest_authentication_errors', 'sumisip_api_request_ips_allowed' );
 
 /**
  * Added role for WordPress.
