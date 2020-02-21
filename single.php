8c3d481533eb6f6512fd17e7a9ba5c9ffab5b461
markherras2003 <larrymark2003@gmail.com>
2020-02-21 17:03:58 +0800

    Update single.php

--- single.php
+++ single.php
@@ -121,10 +121,11 @@ $i=0;
 </div>
 
 
-
+<?php $author_ID = get_the_author_meta( 'ID' ); ?>
+<?php if ($author_ID!=1) {?>
 <div class="author-profile">
     <div class="author-img">
-        <?php $author_ID = get_the_author_meta( 'ID' ); ?>
+      
         
         <?= get_avatar($author_ID,360);
         ?>
@@ -143,7 +144,7 @@ $i=0;
             }
         ?>
     </div>
-    <?php if ($author_ID!=1) {?>
+    
     <div class="author-details">
     <span>About the Author</span>
         <h4 class="author-name"><?= $full_name; ?></h4>
@@ -152,9 +153,8 @@ $i=0;
             ?>
             <p><?= $userMeta['description'][0] ?></p>
     </div>
-    <?php } ?>
 </div>
-
+<?php } ?>
 
 <div class="post-social-media">
     <div class="social-wrapper">
